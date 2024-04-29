e496b0bb94a7eb6337b44165c7a84f2d8246ae0d
Magnus <magnus.uno.friberg@gmail.com>
2024-04-29 12:40:21 +0200

    Api with frontend

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 # Use an official lightweight Python image.
-FROM python:3.10-slim
+FROM python:3.11-slim
 
 # Set the working directory in the container
 WORKDIR /app
+
+# Install system dependencies for OpenCV
+RUN apt-get clean && apt-get update && apt-get install -y \
+    libglib2.0-0 \
+    libgl1-mesa-dev
 
 # Copy the requirements file and install Python dependencies.
 COPY requirements.txt ./
--- api/main.py
+++ api/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI, HTTPException, File, UploadFile
+from fastapi import FastAPI, HTTPException, File, UploadFile, HTTPException, BackgroundTasks
+from fastapi.responses import JSONResponse
 from google.cloud import storage
-import os
+from .image_upscaling import upscale
+import numpy as np
+import cv2
+import uuid
 from io import BytesIO
 from PIL import Image
 from pydantic import BaseModel
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 app = FastAPI()
@@ -34,6 +41,22 @@ def upload_image(image: Image, file_name: str):
     blob.upload_from_file(buffer, content_type="image/jpeg")
     return blob.public_url
 
+def process_and_upload_image(file_data, image_id):
+    try:
+        # Convert the image data to a NumPy array
+        nparr = np.frombuffer(file_data, np.uint8)
+        img_lq = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        img_lq = img_lq.astype(np.float32) / 255.0
+        processed_image = upscale(img_lq)
+
+        # send image to cloud storage
+        processed_image = (processed_image * 255.0).round().astype(np.uint8)
+        processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
+        processed_image_url = upload_image(processed_image, f"{image_id}.jpg")
+        logging.info(f"Image processed and uploaded: {processed_image_url}")
+    except Exception as e:
+        logging.error(f"Error processing image in background: {e}")
+
 @app.post("/process-image/")
 async def process_image(image_url: ImageRequest):
     try:
@@ -48,3 +71,16 @@ async def process_image(image_url: ImageRequest):
         return {"message": "Image processed successfully", "url": transformed_image_url}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+@app.post("/upscale/")
+async def upscale_image(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+    if not file.filename.endswith(('.png', '.jpg', '.jpeg')):
+        raise HTTPException(status_code=400, detail="Invalid file format")
+
+    image_id = str(uuid.uuid4())  # Generate a unique ID for the image process
+    image_data = await file.read()
+    # Schedule the processing of the image as a background task
+    background_tasks.add_task(process_and_upload_image, image_data, image_id)
+
+    # Immediately return a response to the client
+    return {"message": "Image is being processed", "image_id": image_id}
